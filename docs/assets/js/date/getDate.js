19c843577690234e013064457f4cd76391f57fd3
SHUTitDOWN <SHUTitDOWN1998@gmail.com>
2023-07-25 21:36:10 -0500

    Unit Testing

---
+++ docs/assets/js/date/getDate.js
@@ -0,0 +1,50 @@
+export function getDate(dateStr) {
+
+	var d = {}
+	dateArray = []
+
+	function nan(a) {
+		if (Number.isNaN(a)) { return "Unknown" } else { return a }
+	}
+
+
+	if (typeof(dateStr) == "string") {
+		if (dateStr.includes("-")) {dateArray = dateStr.split("-")} else {dateArray[0] = dateStr}
+	}
+	
+
+	var dateLen = dateArray.length
+
+	switch (dateLen) {
+		case 3:
+			d.year = parseInt(dateArray[2]);
+			d.month = parseInt(dateArray[0]);
+			d.day = parseInt(dateArray[1]);
+
+			d.year = nan(d.year)
+			d.month = nan(d.month)
+			d.day = nan(d.day)
+			break;
+
+		case 2:
+			d.year = parseInt(dateArray[1]);
+			d.month = parseInt(dateArray[0]);
+
+			d.year = nan(d.year);
+			d.month = nan(d.month);
+			break;
+
+		case 1:
+
+			d.year = parseInt(dateArray[0]);
+			d.year = nan(d.year);
+			break;
+
+		case '':
+			d.year = "Unknown";
+			break;
+	}
+
+	return d
+
+}
